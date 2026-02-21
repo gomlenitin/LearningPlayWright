5286171b13f8e1d47e4bd3b99ae23888fdecd948
gomlenitin <gomlenitin@gmail.com>
2026-02-22 02:19:25 +0530

    Add Chapter 4: Operators and Chapter 5: Statements

---
+++ chapter_05_Statements/33_If_Statement.js
@@ -0,0 +1,15 @@
+let age = 20;
+
+if (age > 18) {
+    console.log("Yes, Goa");
+} else {
+    console.log("Go home!");
+}
+
+let age1 = 20;
+
+if (age1 > 18) {
+    console.log("Yes, Goa");
+} else {
+    console.log("Go home!");
+}
---
+++ chapter_05_Statements/39_IQ.js
@@ -0,0 +1,10 @@
+// we can run one statement without brackets
+let x = 10;
+if (x > 5)
+    console.log("x is big");
+
+// we cannot run two statement without brackets
+if (x > 5) {
+    console.log("x is big");
+    console.log("x is big");
+}
